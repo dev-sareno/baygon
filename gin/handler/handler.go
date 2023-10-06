b9b1e67396df34390399889ed0cd5e80c6c12955
dev-sareno <dev.sareno@gmail.com>
2023-10-07 03:54:31 +0800

    chore(gin): Add POST /

--- gin/handler/handler.go
+++ gin/handler/handler.go
@@ -2,6 +2,9 @@ package handler
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/dev-sareno/ginamus/gin/db"
 	"github.com/dev-sareno/ginamus/gin/mq"
 	"github.com/gin-gonic/gin"
 	"github.com/rabbitmq/amqp091-go"
@@ -12,28 +15,51 @@ import (
 )
 
 func Post(c *gin.Context, ch *amqp091.Channel) {
+	type Request struct {
+		Domains []string `json:"domains" binding:"required"`
+	}
+
+	var body Request
+	if err := c.ShouldBind(&body); err != nil {
+		c.String(http.StatusBadRequest, fmt.Sprintf("Bad request %s", err.Error()))
+		return
+	}
+
+	fmt.Println(body.Domains)
+
+	d, err := json.Marshal(&body.Domains)
+	if err != nil {
+		c.String(http.StatusInternalServerError, fmt.Sprintf("Server error %s", err))
+		return
+	}
+
+	if err := db.CreateJob(body.Domains); err != nil {
+		c.String(http.StatusInternalServerError, fmt.Sprintf("Server error %s", err))
+		return
+	}
+
 	q, err := ch.QueueDeclare(
-		"hello", // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
+		"jobs", // name
+		false,  // durable
+		false,  // delete when unused
+		false,  // exclusive
+		false,  // no-wait
+		nil,    // arguments
 	)
 	mq.FailOnError(err, "Failed to declare a queue")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	body := "Hello World!"
+	mqbody := string(d)
 	err = ch.PublishWithContext(ctx,
 		"",     // exchange
 		q.Name, // routing key
 		false,  // mandatory
 		false,  // immediate
 		amqp.Publishing{
-			ContentType: "text/plain",
-			Body:        []byte(body),
+			ContentType: "application/json",
+			Body:        []byte(mqbody),
 		})
 	mq.FailOnError(err, "Failed to publish a message")
 	log.Printf(" [x] Sent %s\n", body)
