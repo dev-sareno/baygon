e7f44730ad337405e8f31747e7bda3f72711d6f8
dev-sareno <dev.sareno@gmail.com>
2023-10-07 01:58:16 +0800

    chore: Add MQ publish

--- gin/handler/handler.go
+++ gin/handler/handler.go
@@ -1,20 +1,46 @@
 package handler
 
 import (
+	"context"
+	"github.com/dev-sareno/ginamus/gin/mq"
 	"github.com/gin-gonic/gin"
+	"github.com/rabbitmq/amqp091-go"
+	amqp "github.com/rabbitmq/amqp091-go"
 	"log"
+	"net/http"
+	"time"
 )
 
-func failOnError(err error, msg string) {
-	if err != nil {
-		log.Panicf("%s: %s", msg, err)
-	}
+func Post(c *gin.Context, ch *amqp091.Channel) {
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	mq.FailOnError(err, "Failed to declare a queue")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	body := "Hello World!"
+	err = ch.PublishWithContext(ctx,
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	mq.FailOnError(err, "Failed to publish a message")
+	log.Printf(" [x] Sent %s\n", body)
+
+	c.String(http.StatusOK, "Ok")
 }
 
-func Post(c *gin.Context) {
-
-}
-
-func Get(c *gin.Context) {
+func Get(c *gin.Context, ch *amqp091.Channel) {
 
 }
--- gin/main.go
+++ gin/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/dev-sareno/ginamus/gin/handler"
+	"github.com/dev-sareno/ginamus/gin/mq"
 	"github.com/gin-gonic/gin"
 	amqp "github.com/rabbitmq/amqp091-go"
-	"log"
 	"net/http"
 	"os"
 )
@@ -13,11 +13,18 @@ import (
 func main() {
 	// setup RabbitMQ
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	if err != nil {
-		log.Panicf("%s: %s", err, "Failed to connect to RabbitMQ")
-	}
+	mq.FailOnError(err, "Failed to connect to RabbitMQ")
 	defer func() {
 		if err := conn.Close(); err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+	}()
+
+	ch, err := conn.Channel()
+	mq.FailOnError(err, "Failed to open a channel")
+	defer func() {
+		if err := ch.Close(); err != nil {
 			fmt.Println(err.Error())
 			os.Exit(1)
 		}
@@ -33,11 +40,11 @@ func main() {
 	})
 
 	r.POST("/", func(c *gin.Context) {
-		handler.Post(c)
+		handler.Post(c, ch)
 	})
 
 	r.GET("/", func(c *gin.Context) {
-		handler.Get(c)
+		handler.Get(c, ch)
 	})
 
 	if err := r.Run(":8000"); err != nil {
